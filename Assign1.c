4c12e51037cf500a5c014014c5b4ee9aa6b95cd7
aadity25 <dknhpc04@gmail.com>
2023-10-19 21:15:31 +0530

    Assignment.1

---
+++ Assign1.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+//Q.1
+/*int main()
+{
+    printf("Hello Student");
+    return 0;
+}*/
+
+//Q.2
+/*int main()
+{
+    printf("Hello\nStudent");
+    return 0;
+}*/
+
+//Q.3
+/*int main()
+{
+    printf("\"MySirG\"");
+    return 0;
+}*/
+
+//Q.4
+/*int main()
+{
+    printf("\\n");
+    return 0;
+}*/
+
+//Q.5
+/*int main()
+{
+    printf("\\r");
+    return 0;
+}*/
+
+//Q.6
+/*int main()
+{
+    printf("\"Teacher's Day\"");
+    return 0;
+}*/
+
+//Q.7
+/*int main()
+{
+    int a,b;
+    printf("Enter two number\n");
+    scanf("%d %d",&a,&b);
+    printf("Sum of %d and %d is %d",a,b,a+b);
+    return 0;
+}*/
+
+//Q.8
+/*int main()
+{
+    int a;
+    printf("Enter the number\n");
+    scanf("%d",&a);
+    printf("Area of Square is %d",a*a);
+    return 0;
+}*/
+
+//Q.9
+/*int main()
+{
+    int h,b;
+    printf("Enter two number\n");
+    scanf("%d %d",&b,&h);
+    printf("Area of rectangle is %d",b*h);
+    return 0;
+}*/
+
+//Q.10
+/*int main()
+{
+    int r;
+    printf("Enter the number\n");
+    scanf("%d",&r);
+    printf("Area of Circle is %f",3.14*r*r);
+    return 0;
+}*/
