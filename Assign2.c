7382c07419dd5a86e7dd9516a6a6a0e18242a072
aadity25 <dknhpc04@gmail.com>
2023-10-19 21:26:57 +0530

    Assing.2

---
+++ Assign2.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+//Q.1
+/*int main()
+{
+    int a,b,c;
+    float avg;
+    printf("Enter three number\n");
+    scanf("%d %d %d",&a,&b,&c);
+    avg=a+b+c/3.0;
+    printf("Avrage of three number is %d",avg);
+    return 0;
+}*/
+
+//Q.2
+/*int main()
+{
+    int r;
+    printf("Enter the value of radius\n");
+    scanf("%d",&r);
+    printf("Circumference of circle is %d",2*3.14*r);
+    return 0;
+}*/
+
+//Q.3
+/*int main()
+{
+    int p,r,t;
+    float SI;
+    printf("Entre the value of p,r and t\n");
+    scanf("%d %d %d",&p,&r,&t);
+    SI=p*r*t/100.0;
+    printf("Simple intrest is %d",SI);
+    return 0;
+}*/
+
+//Q.4
+/*int main()
+{
+    int l,b,h;
+    printf("Enter the value\n");
+    scanf("%d %d %d",&l,&b,&h);
+    printf("Volume of cuboid is %d",l*b*h);
+    return 0;
+}*/
+
+//Q.5
+/*int main()
+{
+  int cp,sp,loss,profit;
+  printf("Enter the cp and sp\n");
+  scanf("%d %d",&cp,&sp);
+  printf("profit is %d and loss Is %d",25*(sp-cp),25*(cp-sp));
+  return 0;
+}*/
