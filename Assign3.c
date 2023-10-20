44e3b3f2a2681e04cd5fb0a98e094217833b8fbc
aadity25 <dknhpc04@gmail.com>
2023-10-20 08:14:46 +0530

    Assign.3

---
+++ Assign3.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+//Q.1
+/*int main()
+{
+    char ch;
+    printf("Enter a chararcter\n");
+    scanf("%c",&ch);
+    printf("ASCII code is %d",ch);
+    return 0;
+}*/
+
+//Q.2
+/*int main()
+{
+    int a;
+    printf("Enter a ASCII code\n");
+    scanf("%d",&a);
+    printf("Chararcter is %c",a);
+    return 0;
+}*/
+
+//Q.3
+/*int main()
+{
+    char a,b,c;
+    printf("Enter three chararter\n");
+    scanf("%c %c %c",&a,&b,&c);
+    printf("Chararter is %c=%d %c=%d %c=%d",a,a,b,b,c,c);
+    return 0;
+}*/
+
+//Q.4
+/*int main()
+{
+    int x;
+    printf("Enter the number\n");
+    scanf("%d",&x);
+    printf("Last digit is %d",x%10);
+    return 0;
+}*/
+
+//Q.5
+/*int main()
+{
+    int x;
+    printf("ENter a number\n");
+    scanf("%d",&x);
+    printf("Without last digit is %d",x/10);
+    return 0;
+}*/
+
