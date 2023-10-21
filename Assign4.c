6906d1facfc4f2c2984419734fe6350627e3c014
aadity25 <dknhpc04@gmail.com>
2023-10-21 08:10:39 +0530

    Assign.4

---
+++ Assign4.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+//Q.1
+/*int main()
+{
+    printf("Size of Chararcter constant is %d",sizeof('A'));
+    return 0;
+}*/
+
+//Q.2
+/*int main()
+{
+    printf("Size of real constant is %d",sizeof(34.5));
+    return 0;
+}*/
+
+//Q.3
+/*int main()
+{
+    char ch='A';
+    ch++;
+    printf("Incremented value is %c",ch);
+    return 0;
+}*/
+
+//Q.4
+/*int main()
+{
+    int a,b,c;
+    printf("Enter two number\n");
+    scanf("%d %d",&a,&b);
+    c=a;
+    a=b;
+    b=c;
+    printf("Value of a is %d and b is %d",a,b);
+    return 0;
+}*/
+
+//Q.5
+/*int main()
+{
+    int a,b;
+    printf("Enter two number\n");
+    scanf("%d %d",&a,&b);
+    printf("Swaped value is %d and %d",(a+b)-a,(a+b)-b);
+    return 0;
+}*/
+
+//Q.6
+/*int main()
+{
+    int a,b;
+    printf("Enter two number\n");
+    scanf("%d %d",&a,&b);
+    printf("Swaped value is %d and %d",(a*b)/a,(a*b)/b);
+    return 0;
+}*/
+
+//Q.7
+int main()
+{
+    int a,b;
+    printf("Enter tha value of a and b\n");
+    scanf("%d %d",&a,&b);
+    
+    printf("The value a is %d and b is  %d",x^a,x^b);
+    return 0;
+}
