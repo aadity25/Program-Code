6979d96638875d1370d43b84165ab05c441aeb60
aadity25 <dknhpc04@gmail.com>
2023-10-21 08:19:34 +0530

    Assign.5

---
+++ Assign5.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+//Q.1
+/*int main()
+{
+    int a,b,c=0;
+    printf("Enter three digit number\n");
+    scanf("%d",&a);
+    b=a%10;
+    c=c+b;
+    a=a/10;
+    b=a%10;
+    c=c+b;
+    a=a/10;
+    c=c+a;
+    printf("Sum of three digit number is %d",c);
+    return 0;
+}*/
+
+//Q.2
+/*int main()
+{
+    char ch='+';
+    printf("ASCII code of character '+' is %d",ch);
+    return 0;
+}*/
+
+//Q.3
+/*int main()
+{
+    int a;
+    float f;
+    char ch;
+    double d;
+    printf("Size of int is %d\n",sizeof(a));
+    printf("Size of float is %d\n",sizeof(f));
+    printf("Size of char is %d\n",sizeof(ch));
+    printf("Size of double is %d",sizeof(d));
+    return 0;
+}*/
+
+//Q.4
+/*int main()
+{
+    int a;
+    printf("Enter the number\n");
+    scanf("%d",&a);
+    printf("The number is %d",a/10*10);
+    return 0;
+}*/
+
+//Q.5
+/*int main()
+{
+    int a,b;
+    printf("Enter a number and digit\n");
+    scanf("%d %d",&a,&b);
+    printf("The number is %d",a*10+b);
+    return 0;
+}*/ 
