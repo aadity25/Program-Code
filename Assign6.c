d3aeb10b606bf357216ed95f6037fc3f7551ea2f
aadity25 <dknhpc04@gmail.com>
2023-10-22 08:54:22 +0530

    Assign.6

---
+++ Assign6.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+//Q.2
+/*int main()
+{
+    int a,b,c;
+    printf("Enter three digit number\n");
+    scanf("%d",&a);
+    b=a%10;
+    c=b*100;
+    a=a/10;
+    b=a%10;
+    c+=b;
+    a=a/10;
+    c+=a*10;
+    printf("Invorted number is %d",c);
+    return 0;
+}*/
+
+//Q.3
+/*int main()
+{
+    int x=10>8>4;
+    printf("Value of x is %d",x);
+    return 0;
+}*/
+
+//Q.4
+/*int main()
+{
+    int x=!2>-2;
+    printf("Value of x is %d",x);
+    return 0;
+}*/
+
+//Q.5
+/*int main()
+{
+    int x=3<0&&5>0;
+    printf("Value of x is %d",x);
+    return 0;
+}*/
