f2870de5d464ab15d03ac9d402b2b7f28a35a4a1
aadity25 <dknhpc04@gmail.com>
2023-10-22 08:58:57 +0530

    Assign.7

---
+++ Assign7.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+//Q.1
+/*int main()
+{
+    int x;
+    printf("Entre the value of x\n");
+    scanf("%d",&x);
+    if(x<=0)
+        printf("non-positive number");
+    else
+        printf("positive number");
+    return 0;    
+}*/
+
+//Q.2
+/*int main()
+{
+    int x;
+    printf("Enter a number\n");
+    scanf("%d",&x);
+    if(x%5==0)
+        printf("number is divisible by 5");
+    else
+        printf("not dvisible by 5");
+    return 0;     
+}*/
+
+//Q.3
+/*int main()
+{
+    int x;
+    printf("Enter a number\n");
+    scanf("%d",&x);
+    if(x%2==0)
+       printf("Even number");
+    else
+       printf("odd number");
+    return 0;      
+}*/
+
+//Q.4
+/*int main()
+{
+    int x;
+    printf("Enter the number\n");
+    scanf("%d",&x);
+    if(x/2*2==x)
+       printf("odd number");
+    else
+       printf("even number");
+    return 0;      
+}*/
+
+//Q.5
+int main
+{
+    
+}
