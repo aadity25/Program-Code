76c2414839406d7af16c9a742cc8c3299ce067f4
aadity25 <dknhpc04@gmail.com>
2023-10-23 09:33:07 +0530

    Assign.9

---
+++ Assign9.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+//Q.1
+/*int main()
+{
+    int cp, sp;
+    float lp,pp;
+    printf("Enter the cp nad sp\n");
+    scanf("%d %d",&cp,&sp); 
+    if(sp>cp)
+    {
+        pp=((sp-cp)*100)/cp;
+        printf("Net profit percentage is  %f",pp);
+    }    
+    else
+    {
+        lp=((cp-sp)*100)/cp;
+        printf("Net loss percentage is %.2f",lp);
+    }
+        
+    return 0;
+}*/
+
+//Q.2
+/*int main()
+{
+    int a,b,c,d,e;
+    printf("Enter the marks of 5 subject of student out of 100\n");
+    scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+    if(a>=33 && a<=100 && b>=33 && b<=100 && c>=33 && c<=100 && d>=33 && d<=100 && e>=33 && e<=100 )
+        printf("Candidate pass");
+    else
+        printf("Candidate failed");
+    return 0;    
+}*/
+
+//Q.3
+/*int main()
+{
+    char ch;
+    printf("Enter the alphabet\n");
+    scanf("%c",&ch);
+    if(ch>=65 && ch<=90)
+        printf("Uppercase");
+    else
+        printf("lowercase");
+    return 0;        
+}*/
+
+//Q.4
+/*int main()
+{
+    int x;
+    printf("Enter a number\n");
+    scanf("%d",&x);
+    if(x%2==0 && x%3==0)
+       printf("Divisible by both");
+    else
+       printf("Not divisible by both");  
+    return 0;   
+}*/
+
+//Q.5
+/*int main()
+{
+    int x;
+    printf("Enter a number\n");
+    scanf("%d",&x);
+    if(x%7==0)
+       printf("Divisible by 7");
+    else if(x%3==0)
+       printf("Divisible by 3");
+    else 
+       printf("Not Divisible by 3 and 7");
+    return 0; 
+}*/
