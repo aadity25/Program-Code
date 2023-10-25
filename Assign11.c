56d0f25eff5fe2d30866451fcf6274a15b176a06
aadity25 <dknhpc04@gmail.com>
2023-10-25 21:53:21 +0530

    Assign.11

---
+++ Assign11.c
@@ -0,0 +1,123 @@
+#include<stdio.h>
+//Q.1
+/*int main()
+{
+    int i=1;
+    while(i<=5)
+    {
+        printf("MySirG\n");
+        i++;
+    }
+    return 0;
+}*/
+
+//Q.2
+/*int main()
+{
+    int i=1;
+    printf("Natural number\n");
+    while(i<=10)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}*/
+
+//Q.3
+/*int main()
+{
+    int i=0;
+    printf("Natural number in Revers order\n");
+    while(i<10)
+    {
+        printf("%d\n",10-i);
+        i++;
+    }
+    return 0;
+}*/
+
+//Q.4 
+/*int main()
+{
+    int i=0;
+    printf("10 Odd Natural number\n");
+    while(i<10)
+    {
+        printf("%d\n",2*i+1);
+        i++;
+    }
+    return 0;
+}*/
+
+//Q.5
+/*int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+       printf("%d\n",21-i*2);
+       i++;
+    }
+    return 0;
+}*/
+
+//Q.6
+/*int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+       printf("%d\n",2*i);
+       i++;
+    }
+    return 0;
+}*/
+
+//Q.7
+/*int main()
+{
+    int i=0;
+    while (i<10)
+    {
+        printf("%d\n",20-i*2);
+        i++;
+    }
+    return 0;
+}*/
+
+//Q.8
+/*int main()
+{
+    int i=1;
+    while (i<=10)
+    {
+        printf("%d\n",i*i);
+        i++;
+    }
+    return 0;
+}*/
+
+//Q.9
+/*int main()
+{
+    int i=1;
+    while (i<=10)
+    {
+        printf("%d\n",i*i*i);
+        i++;
+    }
+    
+}*/
+
+//Q.10
+/*int main()
+{
+    int n=5,i=1;
+    while (i<=10)
+    {
+        printf("%d X %d = %d\n",n,i,n*i);
+        i++;
+    }
+    return 0;
+}*/
