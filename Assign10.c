2f0aea5b87cf9e888f3b977751a0584d8a17e277
aadity25 <dknhpc04@gmail.com>
2023-10-25 21:45:20 +0530

    Assign.10

---
+++ Assign10.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+/*int main()
+{
+    int x;
+    printf("Enter a number\n");
+    scanf("%d",&x);
+    if(x>=1)
+       printf("positive number");
+    else if(x<0)
+      printf("Negative number");
+    else
+      printf("Zero");
+    return 0;       
+}*/
+
+//Q.2
+/*int main()
+{
+    char ch;
+    printf("Enter the character\n");
+    scanf("%c",&ch);
+    if(ch>=65 && ch<=90)
+    {
+        printf("Uppercase");
+    }
+    else if(ch>=97 && ch<=122)
+    {
+        printf("Lowercase");
+    }
+    else if (ch>=48 && ch<=57)
+    {
+        printf("Digit");
+    }
+    else
+    {
+        printf("Special chararcter");
+    }
+    return 0; 
+}*/
+
+//Q.3
+/*int main()
+{
+    int a,c,b;
+    printf("Enter the three side of traingle\n");
+    scanf("%d %d %d",&a,&b,&c);
+    if(c>a+b || b>a+c || a>b+c)
+    {
+        printf("Valid traingle");
+    }
+    else
+    {
+        printf("Not valid traingle");
+    }
+    return 0;
+}*/
+
+//Q.4
+/*int main()
+{
+    int x;
+    printf("Enter a vaild month number between 1 to 12");
+    scanf("%d",&x);
+    if(x=1)
+      printf("january with 31 days");
+     else if(x==2)
+      printf("February with 28 or (29 in leap year)days");
+    else if(x==3)
+      printf("March with 31days");
+    else if(x==4)
+      printf("April with 30days");
+    else if(x==5)
+      printf("May with 31 days");
+    else if(x==6)
+      printf("June with 30 days");
+    else if(x==7)
+      printf("July with 31 days");
+    else if(x==8)
+      printf("August with 31 days");
+    else if(x==9)
+      printf("September with 30days");
+    else if(x==10)
+      printf("October with 31 days"); 
+    else if(x==11)
+      printf("November with 30 days");
+    else           
+      printf("December with 30 days");
+    return 0;
+}*/
