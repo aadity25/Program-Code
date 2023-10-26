d97d443005603ffffeb9db4a36737f8ae502f502
aadity25 <dknhpc04@gmail.com>
2023-10-26 19:30:55 +0530

    Assign.13

---
+++ Assign13.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+//Q.1
+/*int main()
+{
+    int i,n,sum=0;
+    printf("Enter the value of n\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+      sum+=i;
+    }
+    printf("Sum of n natural number is %d",sum);
+    return 0;
+}*/
+
+//Q.2
+/*int main()
+{
+    int n,i,sum=0;
+    printf("Enter the value of n\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        sum+=2*i;
+    }
+    printf("Sum of n even natural number is %d",sum);
+    return 0;
+}*/
+
+//Q.3
+/*int main()
+{
+    int i,n,sum=0;
+    printf("Enter the value of n\n");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        sum+=2*i+1;
+    }
+    printf("Sum of n odd natural number is %d",sum);
+    return 0;
+}*/
+
+//Q.4
+/*int main()
+{
+    int n,i,sum;
+    printf("Enter the value of n\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        sum+=i*i;
+    }
+    printf("Sum of square number is %d",sum);
+    return 0;
+}*/
+
+//Q.5
+/*int main()
+{
+    int i,n,sum;
+    printf("Enter the value of n\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+       sum+=i*i*i;
+    }
+    printf("Sum of cube number is %d",sum);
+    return 0;
+}*/
