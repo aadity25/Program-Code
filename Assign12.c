12ceafc9df965d649c42b631f57f481a03aafafc
aadity25 <dknhpc04@gmail.com>
2023-10-26 19:27:56 +0530

    Assign.12

---
+++ Assign12.c
@@ -0,0 +1,140 @@
+#include<stdio.h>
+//Q.1
+/*int main()
+{
+    int i=1,n;
+    printf("Enter the value of n\n");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("MySirG\n");
+        i++;
+    }
+    return 0;
+}*/
+
+//Q.2
+/*int main()
+{
+    int i=1,n;
+    printf("Enter the value of n\n");
+    scanf("%d",&n);
+    while (i<=n)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}*/
+
+//Q.3
+/*int main()
+{
+    int i=0,n;
+    printf("Enter the value of n\n");
+    scanf("%d",&n);
+    while (i<n)
+    {
+        printf("%d\n",n-i);
+        i++;
+    }
+    return 0;
+}*/
+
+//Q.4
+/*int main()
+{
+    int i=0,n;
+    printf("Enter the value of n\n");
+    scanf("%d",&n);
+    while(i<n)
+    {
+        printf("%d\n",2*i+1);
+        i++;
+    }
+    return 0;
+}*/
+
+//Q.5
+/*int main()
+{
+    int i=0,n;
+    printf("Enter the value of n\n");
+    scanf("%d",&n);
+    while (i<n)
+    {
+        printf("%d\n",20-(i+(i+1)));
+        i++;
+    }
+    return 0;
+}*/
+
+//Q.6
+/*nt main()
+{
+    int i=1,n;
+    printf("Enter the value of n\n");
+    scanf("%d",&n);
+    while (i<=n)
+    {
+        printf("%d\n",i+i);
+        i++;
+    }
+    
+}*/
+
+//Q.7
+/*int main()
+{
+    int i=0,n;
+    printf("Enter the value of n\n");
+    scanf("%d",&n);
+    while (i<n)
+    {
+         printf("%d\n",20-i*2);
+         i++;
+    }
+    return 0;
+}*/
+
+//Q.8
+/*int main()
+{
+    int i=1,n;
+    printf("Enter the value of n\n");
+    scanf("%d",&n);
+    while (i<=n)
+    {
+        printf("%d",i*i);
+        i++;
+    }
+    return 0;
+}*/
+
+//Q.9
+/*int main()
+{
+    int i=1,n;
+    printf("Enter the value of n\n");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("%d\n",i*i*i);
+        i++;
+    }
+    return 0;
+}*/
+
+//Q.10
+/*int main()
+{
+    int i=1,n;
+    printf("Enter the value of n\n");
+    scanf("%d",&n);
+    while (i<=10)
+    {
+        printf("%d x %d =%d\n",n,i,n*i);
+        i++;
+    }
+    return 0;
+}*/
